0fcb5ea9754870fa3e313696da67da28f436b1d3
Ethan Smith <github@ethansmith.io>
2022-01-27 18:51:30 +0000

    Fix index out of bounds on BTree

--- BinaryTree.ts
+++ BinaryTree.ts
@@ -4,14 +4,14 @@ import Cell from './Cell';
 export default class BinaryTree {
     static on(grid: Grid): Grid {
         grid.getCells().forEach((cell: Cell) => {
-            const neighbors = new Array();
+            const neighbors = [];
             cell.north ? neighbors.push(cell.north) : null;
             cell.east ? neighbors.push(cell.east) : null;
 
-            const idx = Math.round(Math.random() * neighbors.length);
+            const idx = Math.round(Math.random() * (neighbors.length - 1));
             const neighbor: Cell = neighbors[idx];
 
-            neighbor ? cell.link(neighbor, true) : null
+            neighbor ? cell.link(neighbor, true) : null;
         });
 
         return grid;
--- BinaryTreeRunner.ts
+++ BinaryTreeRunner.ts
@@ -5,6 +5,6 @@ import Cell from './Cell';
 const grid = new G(4, 4);
 BT.on(grid);
 console.log(String(grid));
-grid.getRows().forEach((row: Cell[]) => {
-    console.log(row);
-});
+// grid.getRows().forEach((row: Cell[]) => {
+//     console.log(row);
+// });
